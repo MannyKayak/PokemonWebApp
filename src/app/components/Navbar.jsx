8f34fcdb8eb1b0ff88b867da1a0b284d86f58853
VirtualRedy <gustavo_alfonso@hotmail.it>
2023-07-19 08:45:08 +0200

    commit not working version

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,14 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react" 
 
 export default function Navbar() {
+    const isUserLoggedIn = true; 
+    const [providers, setProviders] = useState(null);
+
+    useEffect (() => {
+        const setProviders = async () => {
+            const response = await getProviders()
+        }
+    }, [])
+
     return (
-        <div>
+        <nav>
             <Link href="/">home</Link>
             <Link href="/pokemonList">Pokemon List</Link>
             <Link href="/pokemonList">Rules</Link>
             <Link href="/pokemonList">Account</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/registration">Register</Link>
-        </div>
+            <div>
+                {isUserLoggedIn ? (
+                    <div>
+                        <Link href="/makeYourTeam">Make Your Team</Link>
+                    </div>
+                ) : (<></>)}
+                <button type="button" onClick={signOut}>
+                    Sign Out
+                </button>
+                {/* will add the link to the profile of the user*/}
+            </div>
+        </nav>
     );
 }
---
+++ src/app/makeYourTeam/page.jsx
@@ -0,0 +1,8 @@
+export default function MakeYourTeam() {
+    return (
+        <div>
+            <h1>Make your team</h1>
+            <p>Here you can choose your starters and with them it time to play!</p>
+        </div>
+    );
+}
